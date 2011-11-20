fa3a272fda8e57812f190076d531271012f732ba
malvika85@gmail.com <malvika85@gmail.com@17077198-2bdb-e9dc-f42e-741e42d500b5>
2011-11-20 15:30:47 +0000

    java process killer and updated log properties file

---
+++ javakiller.sh
@@ -0,0 +1,7 @@
+ssh lin114-01.cise.ufl.edu skill java
+ssh lin114-02.cise.ufl.edu skill java
+ssh lin114-03.cise.ufl.edu skill java
+ssh lin114-04.cise.ufl.edu skill java
+ssh lin114-05.cise.ufl.edu skill java
+ssh lin114-06.cise.ufl.edu skill java
+skill java
